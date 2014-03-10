372f4a9b02f523bcd9d8f81bb73af2110829b1db
delanni <delanni.alex@gmail.com>
2014-03-10 18:27:18 +0100

    Perlin

--- src/CatcherInTheRay/scripts/perlinnoise.js
+++ src/CatcherInTheRay/scripts/perlinnoise.js
@@ -1,16 +1,23 @@
- //CREDITS to Jérémy Bouny : https://github.com/jbouny/terrain-generator/blob/master/generators/perlinnoise.js
+// CREDITS to Jérémy Bouny : https://github.com/jbouny/terrain-generator/blob/master/generators/perlinnoise.js
+// Mutation by Alex
 
 var PN_GENERATOR =
-{ 
-  RandomNoise: function( inParameters, inCanvas, inX, inY, inWidth, inHeight, inAlpha ) 
+{
+  RandomNoise: function(inParameters, inCanvas) 
   {   
     var g = inCanvas.getContext("2d"),
       imageData = g.getImageData( 0, 0, inCanvas.width, inCanvas.height ),
       pixels = imageData.data;
 
-    for( var i = 0; i < pixels.length; i += 4 )
+    for(var i = 0; i < pixels.length; i += 4 )
     {
-      pixels[i] = pixels[i+1] = pixels[i+2] = ( inParameters.random.Random() * 256 ) | 0;
+/*      pixels[i] = ( inParameters.random.Random() * 256 ) | 0;
+      pixels[i+1] = ( inParameters.random.Random() * 256 ) | 0;
+      pixels[i+2] = ( inParameters.random.Random() * 256 ) | 0;*/
+
+      pixels[i] = pixels[i+1] =pixels[i+2] =  ( inParameters.random.Random() * 256 ) | 0;
+
+      /*pixels[i] = pixels[i+1] = pixels[i+2] = ( inParameters.random.Random() * 256 ) | 0;*/
       pixels[i+3] = 255;
     }
 
@@ -18,18 +25,18 @@ var PN_GENERATOR =
     return inCanvas;
   },
 
-  PerlinNoise: function( inParameters )
+  PerlinNoise: function(inParameters , canvas)
   {
     /**
      * This part is based on the snippest :
      * https://gist.github.com/donpark/1796361
      */
 
-    var noise = this.RandomNoise( inParameters, CreateCanvas( inParameters.widthSegments, inParameters.heightSegments ) );
+    var noise = this.RandomNoise( inParameters, canvas);
     var context = inParameters.canvas.getContext("2d");
     context.save();
 
-    var ratio = inParameters.widthSegments / inParameters.heightSegments;
+    var ratio = inParameters.width / inParameters.height;
     
     /* Scale random iterations onto the canvas to generate Perlin noise. */
     for( var size = 4; size <= noise.height; size *= inParameters.param ) 
@@ -37,20 +44,20 @@ var PN_GENERATOR =
       var x = ( inParameters.random.Random() * ( noise.width - size ) ) | 0,
         y = ( inParameters.random.Random() * ( noise.height - size ) ) | 0;
       context.globalAlpha = 4 / size;
-      context.drawImage( noise, Math.max( x, 0 ), y, size * ratio, size, 0, 0, inParameters.widthSegments, inParameters.heightSegments );
+      context.drawImage(noise, Math.max( x, 0 ), y, size * ratio, size, 0, 0, inParameters.width, inParameters.height);
     }
  
     context.restore();
 
-    return inParameters.canvas;
+    return canvas;
   },
 
-  Get: function( inParameters )
+  Generate: function(inParameters)
   {
-    inParameters.param = Math.max( 1.1, inParameters.param );
-
+    inParameters.param = Math.max( 1.1, inParameters.param||null );
+    var canvas = this.CreateCanvas( inParameters.width, inParameters.height ) ;
     // Create the Perlin Noise
-    var noise = this.PerlinNoise( inParameters );
+    var noise = this.PerlinNoise(inParameters,canvas);
 
     return noise;
   },
